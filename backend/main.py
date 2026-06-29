aa7bf4173f338cda98f12d6c9cccf64de05c5e84
Mir Rezwan Navid <m.rezwannavid@gmail.com>
2026-06-29 18:23:27 +0600

    Update CORS origins to allow Vercel webapp

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://world-cup-difficulty.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
